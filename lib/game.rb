c572897554217016246aa40ee0f21a1801f76e29
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-26 19:23:51 -0400

    game is functional, missing save

--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,19 @@
 
 # Holds the game logic
 class Game
-  attr_reader :secret_word
+  attr_reader :secret_word, :game_over
 
   def initialize(possible_words)
     @secret_word = nil
     @possible_words = possible_words
+    @underscores = []
     @incorrect_guesses = 6
+    @guess = nil
+    @game_over = false
   end
 
   def choose_secret_word
     @secret_word = @possible_words.sample
-    p @secret_word
   end
 
   def lowercase_secret_word
@@ -23,19 +25,55 @@ class Game
     @secret_word = @secret_word.split('')
   end
 
-  def compare_guess_to_secret_word(guess)
-    if @secret_word.include?(guess)
-      get_index_of_matches(guess)
-    else @incorrect_guesses -= 1
+  def guess_letter
+    puts 'Guess a letter from a-z. :)'
+    @guess = gets.chomp.downcase
+  end
+
+  def compare_guess_to_secret_word
+    if @secret_word.include?(@guess)
+      index_of_matches
+    else
+      puts 'You guessed wrong!'
+      @incorrect_guesses -= 1
     end
   end
 
-  def get_index_of_matches(guess)
+  def index_of_matches
     indexes = (0...@secret_word.length).find_all do |i|
-      @secret_word[i] == guess
+      @secret_word[i] == @guess
     end
     replace_underscore_with_matches(indexes)
   end
 
-  def replace_underscore_with_matches(num); end
+  def replace_underscore_with_matches(indexes)
+    indexes.each do |i|
+      @underscores[i] = @guess
+    end
+  end
+
+  def replace_secret_word_with_underscores
+    length = @secret_word.length
+    length.times { @underscores.push('_ ') }
+  end
+
+  def display_underscores
+    p @underscores.join
+  end
+
+  def display_result
+    p "#{@incorrect_guesses} incorrect guesses left."
+    p @underscores.join
+  end
+
+  def verify_next_turn
+    @game_over = true if @incorrect_guesses.zero? || @underscores == @secret_word
+  end
+
+  def final_statement
+    if @incorrect_guesses.zero?
+      puts 'You lost!'
+    else puts 'You won!'
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -5,21 +5,27 @@ require_relative 'lib/dictionary'
 require_relative 'lib/player'
 require_relative 'lib/board'
 
+# Load dictionary
 dictionary = Dictionary.new
 dictionary.load_dictionary
 dictionary.read_dictionary
 dictionary.keep_5_to_12_words
 
+# Initial setup
 game = Game.new(dictionary.possible_words)
 game.choose_secret_word
 game.lowercase_secret_word
 game.secret_word_array
+game.replace_secret_word_with_underscores
+game.display_underscores
 
-board = Board.new
-board.replace_secret_word_with_underscores(game.secret_word.length)
-board.display_underscores
+# Game starts
+until game.game_over == true
+  game.guess_letter
+  game.compare_guess_to_secret_word
+  game.display_result
+  game.verify_next_turn
+end
 
-player = Player.new
-player.guess_letter
-
-game.compare_guess_to_secret_word(player.guess)
+# Game Over
+game.final_statement
