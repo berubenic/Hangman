19b91b173f8a30af1c24dfef7b52f54355b6e4a7
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-26 18:56:01 -0400

    find indexes of matches guess

--- lib/game.rb
+++ lib/game.rb
@@ -7,10 +7,12 @@ class Game
   def initialize(possible_words)
     @secret_word = nil
     @possible_words = possible_words
+    @incorrect_guesses = 6
   end
 
   def choose_secret_word
     @secret_word = @possible_words.sample
+    p @secret_word
   end
 
   def lowercase_secret_word
@@ -19,11 +21,21 @@ class Game
 
   def secret_word_array
     @secret_word = @secret_word.split('')
-    p @secret_word
   end
 
   def compare_guess_to_secret_word(guess)
-    p @secret_word
-    p @secret_word.include?(guess)
+    if @secret_word.include?(guess)
+      get_index_of_matches(guess)
+    else @incorrect_guesses -= 1
+    end
   end
+
+  def get_index_of_matches(guess)
+    indexes = (0...@secret_word.length).find_all do |i|
+      @secret_word[i] == guess
+    end
+    replace_underscore_with_matches(indexes)
+  end
+
+  def replace_underscore_with_matches(num); end
 end
